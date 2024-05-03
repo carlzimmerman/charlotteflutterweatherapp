b30ef6d99c9ab64d82ebc42e9d81b887a1d5e404
Carl Zimmerman <carlpzimmerman@gmail.com>
2024-05-03 11:06:19 -0400

    fixes the weather api service function

--- lib/services/weather_api_service.dart
+++ lib/services/weather_api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class WeatherApiService {
   static Future<Map<String, dynamic>> fetchWeather(double lat, double lon) async {
-    var apikey = String.fromEnvironment('WEATHER_API_KEY');
+    var apikey = "WEATHER_API_KEY";
 
     var response = await http.get(Uri.parse('https://api.weatherapi.com/v1/current.json?key=${apikey}&q=$lat,$lon'));
     if (response.statusCode == 200) {
